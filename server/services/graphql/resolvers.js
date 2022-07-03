d1c9134ff88695648eb6e942d5b6590293f8fe63
airoble-1 <airoble2019@gmail.com>
2022-07-03 15:14:15 -0400

    Add graphql query and mutation for posts

--- server/services/graphql/resolvers.js
+++ server/services/graphql/resolvers.js
@@ -1,7 +1,37 @@
+let posts = [
+  {
+    id: 2,
+    text: "Lorem ipsum",
+    user: {
+      avatar: "./images/avatar1.png",
+      username: "Test User",
+    },
+  },
+  {
+    id: 1,
+    text: "Lorem ipsum",
+    user: {
+      avatar: "./images/avatar2.png",
+      username: "Test User 2",
+    },
+  },
+];
+
 const resolvers = {
   RootQuery: {
     posts(root, args, context) {
-      return [];
+      return posts;
+    },
+  },
+  RootMutation: {
+    addPost(root, { post, user }, context) {
+      const PostObject = {
+        ...post,
+        user,
+        id: posts.length + 1,
+      };
+      posts.push(PostObject);
+      return PostObject;
     },
   },
 };
--- server/services/graphql/schema.js
+++ server/services/graphql/schema.js
@@ -4,12 +4,28 @@ const typeDefs = gql`
   type Post {
     id: Int
     text: String
+    user: User
+  }
+  type User {
+    avatar: String
+    username: String
+  }
+  input PostInput {
+    text: String!
+  }
+  input UserInput {
+    avatar: String!
+    username: String!
   }
   type RootQuery {
     posts: [Post]
   }
+  type RootMutation {
+    addPost(post: PostInput!, user: UserInput!): Post
+  }
   schema {
     query: RootQuery
+    mutation: RootMutation
   }
 `;
 export default [typeDefs];
